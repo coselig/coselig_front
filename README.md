0f1a4ef40f10bd5371dae34da9161d8ffa8949be
coselig <coseligtest@gmail.com>
2025-12-16 10:37:19 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# coselig_front
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/coselig_front/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.coselig_front
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
